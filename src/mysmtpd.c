9ee5909ba2956bf9ff643f20b833210eac409fea
samanthahsu <shsu877@gmail.com>
2022-03-29 11:13:22 -0700

    quit, noop

--- src/mysmtpd.c
+++ src/mysmtpd.c
@@ -46,7 +46,7 @@ void handle_client(int fd)
         char *parts[(MAX_LINE_LENGTH + 1) / 2];
 
         int splitTimes = split(recvbuf, parts);
-        if (splitTimes == 1) {
+        if (splitTimes == 0) {
             continue;
         }
 
