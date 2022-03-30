8f3b02a344c29cae97f59709c2ea3e343661e442
Gale Chen <61856642+glchn@users.noreply.github.com>
2022-03-30 12:30:40 -0700

    greeting message working
    added part of quit too

--- src/mypopd.c
+++ src/mypopd.c
@@ -12,6 +12,8 @@
 #define MAX_LINE_LENGTH 1024
 
 static void handle_client(int fd);
+int authstate;
+int transstate;
 
 int main(int argc, char *argv[]) {
   
@@ -28,10 +30,8 @@ int main(int argc, char *argv[]) {
 void greeting(int fd, struct utsname name) {
     uname(&name);
     char *welcome_msg = "+OK %s POP3 server ready\r\n";
-    int error = send_formatted(fd, welcome_msg, name.nodename);
-    if (error == -1) {
-        return;
-    }
+    send_formatted(fd, welcome_msg, name.nodename);
+    authstate = 1;
 }
 
 void handle_client(int fd) {
@@ -42,6 +42,33 @@ void handle_client(int fd) {
     /* TO BE COMPLETED BY THE STUDENT */
     struct utsname my_uname;
     uname(&my_uname);
-    greeting(fd, my_uname);
+
+    char username[MAX_USERNAME_SIZE];
+    char password[MAX_PASSWORD_SIZE];
+    authstate = 0; // start: not authorized
+    transstate = 0; // start: not in transaction state
+
+    greeting(fd, my_uname); // send greeting
+
+    while (nb_read_line(nb, recvbuf)) { // command reading and parsing
+        char *parts[(MAX_LINE_LENGTH + 1) / 2];
+        if (split(recvbuf, parts) <= 0) {
+            continue;
+        }
+
+        char * command = parts[0];
+        dlog("%s\n", command);
+
+        if (strcasecmp("USER", command) == 0) { // USER
+        // do something
+            }
+
+        if (strcasecmp("QUIT", command) == 0) {
+            // load_user_mail(user); <- only do this in auth state
+            // destroy_mail_list(mail_list);
+            char * msg = "+OK %s POP3 server signing off \r\n";
+            send_formatted(fd, msg);
+        }
+    }
     nb_destroy(nb);
 }
