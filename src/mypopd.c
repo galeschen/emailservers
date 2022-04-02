5677f6e8ebb1e70919633c7a793635520d3a068f
Gale Chen <61856642+glchn@users.noreply.github.com>
2022-04-01 20:46:57 -0700

    quit, user
    user partially functional

--- src/mypopd.c
+++ src/mypopd.c
@@ -12,9 +12,8 @@
 #define MAX_LINE_LENGTH 1024
 
 static void handle_client(int fd);
-int authstate;
-int transstate;
-
+int state; // 0 = initial, 1 = authorized, 2 = transaction, 3 = update
+int userentered; // 0 = not entered, 1 = entered
 int main(int argc, char *argv[]) {
   
     if (argc != 2) {
@@ -29,9 +28,22 @@ int main(int argc, char *argv[]) {
 // send greeting
 void greeting(int fd, struct utsname name) {
     uname(&name);
-    char *welcome_msg = "+OK %s POP3 server ready\r\n";
+    char *welcome_msg = "+OK POP3 server ready\r\n";
     send_formatted(fd, welcome_msg, name.nodename);
-    authstate = 1;
+    state = 1;
+}
+
+void destroymail(mail_list_t mail) {
+    reset_mail_list_deleted_flag(mail);
+    destroy_mail_list(mail);
+}
+
+void handlequit(int state, mail_list_t mail, int fd, struct utsname name) {
+    if (state == 2) {
+        destroy_mail_list(mail);
+        }
+        char *msg = "+OK POP3 server signing off\r\n";
+        send_formatted(fd, msg, name.nodename);
 }
 
 void handle_client(int fd) {
@@ -42,32 +54,90 @@ void handle_client(int fd) {
     /* TO BE COMPLETED BY THE STUDENT */
     struct utsname my_uname;
     uname(&my_uname);
-
-    char username[MAX_USERNAME_SIZE];
-    char password[MAX_PASSWORD_SIZE];
-    authstate = 0; // start: not authorized
-    transstate = 0; // start: not in transaction state
+    state = 0; // start: not authorized
+    userentered = 0; // start: no username entered
+    // int mailcount = 0;
+    char *username = NULL;
+    char *password = NULL;
+    mail_list_t mail = NULL;
 
     greeting(fd, my_uname); // send greeting
 
-    while (nb_read_line(nb, recvbuf)) { // command reading and parsing
+    while (1) {
+        int readlineVal;
+        readlineVal = nb_read_line(nb, recvbuf);
+
+        //  -1 is error, 0 is we are done
+        if (readlineVal == -1) {
+            char *msg = "-ERR\r\n";
+            send_formatted(fd, msg, my_uname.nodename);
+            if (mail != NULL) {
+                destroymail(mail);
+            }
+            break;
+        }
+        if (readlineVal == 0) {
+            char *msg = "+OK\r\n";
+            send_formatted(fd, msg, my_uname.nodename);
+            if (mail != NULL) {
+                destroymail(mail);
+            }
+            break;
+        }
+
         char *parts[(MAX_LINE_LENGTH + 1) / 2];
-        if (split(recvbuf, parts) <= 0) {
+
+        int splitCount = split(recvbuf, parts);
+        dlog("%i\n", splitCount * 1);
+        
+        if (splitCount <= 0) {
             continue;
         }
 
         char * command = parts[0];
         dlog("%s\n", command);
+        
+        if (strcasecmp("QUIT", command) == 0) { // can be given in any state
+            handlequit(state, mail, fd, my_uname);
+            break;
+        }
 
-        if (strcasecmp("USER", command) == 0) { // USER
-        // do something
+        if (strcasecmp("NOOP", command) == 0) {
+            send_formatted(fd, "+OK\r\n");
+        }
+
+        if (state == 1) { // two options in authorization (1) state: user and pass
+            if (strcasecmp("USER", command) == 0) {
+                if (is_valid_user(parts[1], NULL) != 0 ) { // username exists
+                    strcpy(username, parts[1]);
+                    send_formatted(fd, "+OK %s is a valid mailbox, enter password\r\n", parts[1]);
+                    parts[1] = '\0';
+                    userentered = 1;
+                } else {
+                    send_formatted(fd, "-ERR never heard of mailbox\r\n");
+                }
             }
 
-        if (strcasecmp("QUIT", command) == 0) {
-            // load_user_mail(user); <- only do this in auth state
-            // destroy_mail_list(mail_list);
-            char * msg = "+OK %s POP3 server signing off \r\n";
-            send_formatted(fd, msg);
+            if (strcasecmp("PASS", command) == 0) {
+                if (userentered == 0) {
+                    send_formatted(fd, "-ERR no username provided\r\n");
+                }
+                if (parts[1] == NULL) {
+                    send_formatted(fd, "-ERR invalid password, enter username again\r\n");
+                    parts[1] = '\0';
+                    username[0] = '\0';
+                    userentered = 0;
+                }
+                if (is_valid_user(username, parts[1]) == 0) {
+                    send_formatted(fd, "+OK matching username and password \r\n");
+                }
+                if (is_valid_user(username, parts[1]) != 0) { //username matches password
+                    mail = load_user_mail(username);
+                    send_formatted(fd, "+OK matching username and password \r\n");
+                    strcpy(password, parts[1]);
+                    state = 2;
+                }
+            }
         }
     }
     nb_destroy(nb);
